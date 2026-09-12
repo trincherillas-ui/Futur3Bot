a810ca7fd3c130603cc09a3eee7fd40aeac0202e
trincherillas-ui <trincherillas@gmail.com>
2026-09-12 10:06:47 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,6 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
+        state: "Comunidad de Futur3Kidd",     // this is what people actually see
         type: 1,               // Streaming
       },
     ],
