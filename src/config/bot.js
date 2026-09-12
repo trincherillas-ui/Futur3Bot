2f11437e84c7add76883f38c6b29cec1cd51863f
trincherillas-ui <trincherillas@gmail.com>
2026-09-12 10:56:52 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Futur3Kidd en Kick", // required by Discord API, not shown in the client
-        state: "",     // this is what people actually see
+        state: "Entra a verle",     // this is what people actually see
         type: 1,               // Streaming
       },
     ],
