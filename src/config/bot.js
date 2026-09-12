6754e90e3d061858be13ff0507661ccb2d552e95
trincherillas-ui <trincherillas@gmail.com>
2026-09-12 10:13:01 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Comunidad de Futur3Kidd",     // this is what people actually see
+        name: "Futur3Kidd en Kick", // required by Discord API, not shown in the client
+        state: "",     // this is what people actually see
         type: 1,               // Streaming
       },
     ],
